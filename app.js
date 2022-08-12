76430b435579c3578a25a406154a1f8dd4b1d5a0
Rodrigo Leon <leonrrodrigo@hotmail.com>
2022-08-12 18:38:05 -0500

    Added check and reset botton funcionality

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const pokemonList = [
   "charmander",
 ];
 
+let answer = "";
+let score = 0;
+let attempts = 0;
+
 //Change the .message text
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
@@ -25,8 +29,9 @@ const displayMessage = function (message) {
 //Start botton funcionality
 btnStart.addEventListener("click", function () {
   //Generate a random pokemon name
+
   const randomName = Math.floor(Math.random() * pokemonList.length);
-  console.log(pokemonList[randomName]);
+  answer = pokemonList[randomName];
 
   //Display pokemon image
   pokemonImage.src = `img/${pokemonList[randomName]}.png`;
@@ -36,6 +41,32 @@ btnStart.addEventListener("click", function () {
 btnCheck.addEventListener("click", function () {
   const guess = document.querySelector(".guess").value;
 
-  if (!guess) displayMessage("No input!");
-  if (guess === pokemonList[pokemonList[randomName]]) displayMessage("Correct");
+  //When is no input
+  if (!guess) {
+    displayMessage("No input!");
+  }
+
+  //When input is correct
+  else if (guess === answer) {
+    displayMessage("Correct");
+    score++;
+    document.querySelector(".score").textContent = score;
+    if (score === 3) displayMessage("Congrats you won!");
+
+    //when input is not correct
+  } else {
+    displayMessage("Ops! Is not correct. Try again");
+    attempts++;
+    document.querySelector(".attempts").textContent = attempts;
+    if (attempts === 3) displayMessage("Sorry :( you lost!");
+  }
+});
+
+//Reset botton funcionality
+btnReset.addEventListener("click", function () {
+  document.querySelector(".score").textContent = 0;
+  document.querySelector(".attempts").textContent = 0;
+  pokemonImage.src = `img/shadow.png`;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
 });
