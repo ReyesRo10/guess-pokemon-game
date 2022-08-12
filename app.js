397fbec6d8177a4137472f2d11b771ed9d6a1b7e
Rodrigo Leon <leonrrodrigo@hotmail.com>
2022-08-12 08:39:14 -0500

    Selecting elements added in JS

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Hola mundo");
+"use strict";
+
+//Selecting elements
+const btnStart = document.querySelector(".start");
+const btnContinue = document.querySelector(".continue");
+const btnReset = document.querySelector(".reset");
+const btnCheck = document.querySelector(".check");
+
+btnStart.addEventListener("click", function () {
+  console.log("Hola mundo");
+});
+
+btnContinue.addEventListener("click", function () {
+  console.log("Hola mundo");
+});
+
+btnReset.addEventListener("click", function () {
+  console.log("Hola mundo");
+});
+
+btnCheck.addEventListener("click", function () {
+  console.log("Hola mundo");
+});
