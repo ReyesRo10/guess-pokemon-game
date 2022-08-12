4387411def73b10ae67e76c6b6780be2454de7d1
Rodrigo Leon <leonrrodrigo@hotmail.com>
2022-08-12 11:22:48 -0500

    Added start botton functionality

--- app.js
+++ app.js
@@ -5,19 +5,37 @@ const btnStart = document.querySelector(".start");
 const btnContinue = document.querySelector(".continue");
 const btnReset = document.querySelector(".reset");
 const btnCheck = document.querySelector(".check");
+const pokemonImage = document.querySelector(".pokemon");
 
+//Pokemon list values
+const pokemonList = [
+  "eevee",
+  "pikachu",
+  "squirtle",
+  "umbreon",
+  "bulbasaur",
+  "charmander",
+];
+
+//Change the .message text
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+//Start botton funcionality
 btnStart.addEventListener("click", function () {
-  console.log("Hola mundo");
+  //Generate a random pokemon name
+  const randomName = Math.floor(Math.random() * pokemonList.length);
+  console.log(pokemonList[randomName]);
+
+  //Display pokemon image
+  pokemonImage.src = `img/${pokemonList[randomName]}.png`;
 });
 
-btnContinue.addEventListener("click", function () {
-  console.log("Hola mundo");
-});
-
-btnReset.addEventListener("click", function () {
-  console.log("Hola mundo");
-});
-
+//Check botton funcionality
 btnCheck.addEventListener("click", function () {
-  console.log("Hola mundo");
+  const guess = document.querySelector(".guess").value;
+
+  if (!guess) displayMessage("No input!");
+  if (guess === pokemonList[pokemonList[randomName]]) displayMessage("Correct");
 });
